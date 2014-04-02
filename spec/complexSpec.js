0517502464bb68f7a78f56db219b33690c5db725
Michal Dorner <dorner.michal@gmail.com>
2014-04-02 02:55:52 +0200

    add "complex" spec

---
+++ spec/complexSpec.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var path = require('path')
+var utils = require('./specUtils');
+var xjade = require('../lib/xjade');
+
+var document = utils.createDocument();
+var fixture = function(name,ext){ return path.join(__dirname,'fixtures','complex',name+'.'+ext); };
+
+
+describe('Complex example', function(){
+
+    it('should generate same html as index.html', function(){
+        var actual = xjade.compile(fixture('index','js-tpl'), {compile: 'html', pretty:true});
+        var expected = fs.readFileSync(fixture('index','html')).toString();
+        expect(actual).toEqual(expected);
+    });
+});
--- spec/specUtils.js
+++ spec/specUtils.js
@@ -4,6 +4,10 @@ exports.createDocument = function() {
     return new xmldom.DOMParser().parseFromString('<!DOCTYPE html>','text/xml');
 }
 
+exports.parse = function(html) {
+    return new xmldom.DOMParser().parseFromString(html,'text/xml');
+}
+
 exports.serialize = function(document) {
-    new xmldom.XMLSerializer().serializeToString(document);
+    return new xmldom.XMLSerializer().serializeToString(document);
 }
