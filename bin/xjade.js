c63605e21cf42c1f076af8b0f2d0ddb3f3ef7814
Michal Dorner <dorner.michal@gmail.com>
2014-04-05 15:21:47 +0200

    simplified cli usage

--- bin/xjade.js
+++ bin/xjade.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var path = require('path');
-var util = require('util');
 var program = require('commander');
 var xjade = require('../lib/xjade');
 
@@ -14,49 +12,27 @@ program
   .option('--doctype <str>", "Doctype: "5"|"strict"|"transitional"|"frameset"|"xhtml"|"xhtml"|"xhtml"|"xhtml"|"basic"|"mobile" or custom', String, '5')
   .option('-l, --locals <str>', 'JSON string to be used as the locals object')
   .option('-p, --pretty', 'Print pretty HTML')
-  .option('--std-out', 'Print result to stdout');
 
 program.parse(process.argv);
 
 var files = program.args;
-var compilerOpts = {};
 
-var compilerExt = {
-  html: 'html',
-  js: 'js',
-  ast: 'txt'
-};
-
-if (program.locals) {
-	compilerOpts.locals = JSON.parse(program.locals);
+var compilerOpts = {
+  compile: program.compile,
+  doctype: program.doctype,
+  pretty: program.pretty,
+  locals: program.locals ? JSON.parse(program.locals) : null,
 }
 
-compilerOpts.compile = program.compile;
-compilerOpts.doctype = program.doctype;
-compilerOpts.pretty = program.pretty;
 
-
-files.forEach(function(filename){
-  var output;
-  var ext = compilerExt[program.compile] || program.compile;
-
-  try {
-    output = xjade.compile(filename, compilerOpts);
-    writeOutput(filename, ext, output);
-  }
-  catch(e) {
-    console.error(e.message || e)
-    process.exit(1)
-  }
-})
-
-
-function writeOutput(filename, ext, data) {
-  if (program.stdOut) {
-    process.stdout.write(data);
-  }
-  else {
-    var fn = filename.replace(/\.[^\/.]+$/,'') + '.' + ext;
-    fs.writeFile(fn, data);
-  }
+try {
+  var output = '';
+  files.forEach(function(filename){
+    output += xjade.compile(filename, compilerOpts);
+  });
+  process.stdout.write(output);
+}
+catch(e) {
+  console.error(e.message || e)
+  process.exit(1)
 }
